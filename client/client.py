88633eb77ccaea0f0fce6cc97fe4d6d33a707d8a
I-Chiao Hsu <ichiao.hsu@gmail.com>
2019-05-14 16:25:56 +0800

    In client use logging to show log. Added error message

--- client/client.py
+++ client/client.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
-import sys
+import sys, requests, logging
 from sample_pb2 import Sample, Activity, Location
-import requests
 from google.protobuf.json_format import Parse
 
-REMOTE_URL = "http://localhost:8000"
+REMOTE_URL = 'http://localhost:8000'
 
 def send_protobuf(filename, remote_url):
     """
-    send_protobuf will open the filename, 
+    send_protobuf will open the filename,
     read each lines, serialize the line to protobuf bytes string,
     and send out as requests body.
     """
@@ -19,22 +18,31 @@ def send_protobuf(filename, remote_url):
             Parse(d, sample)
 
             # Send out the protobuf message
-            requests.post(remote_url, data=sample.SerializeToString(), headers={'Content-type': 'application/x-protobuf'})
+            r = requests.post(remote_url, data=sample.SerializeToString(), headers={'Content-type': 'application/x-protobuf'})
+            # If not POSTed successfully, show error message
+            if r.status_code != 201:
+                logging.error('{} - {}'.format(r.status_code, r.text))
+            else:
+                logging.info('{} - {}'.format(r.status_code, r.text))
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+
+    # Set logging level INFO
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s')
 
     # Prompt user to filename
     # If there should be no filename, exit
-    filename = input("Enter filename: ")
+    filename = input('Enter filename: ')
     if len(filename) == 0:
-        print("Filename is empty!")
+        logging.warning('Filename is empty')
         sys.exit(1)
-    
+
     # Prompt user for server address
     # if there is none, use default REMOTE_URL
-    remote_url = input("Enter remote server address: ")
+    remote_url = input('Enter remote server address: ')
     if len(remote_url) == 0:
-        remote_url = REMOTE_URL 
+        logging.info('Remote url is empty. Use default localhost:8000')
+        remote_url = REMOTE_URL
 
     # Use the filename and remote address to send protobuf
     send_protobuf(filename, remote_url)
